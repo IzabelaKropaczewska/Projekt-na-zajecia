d3aa1465858954e6db57e3849633b495d3c31ed4
IzabelaKropaczewska <83338557+IzabelaKropaczewska@users.noreply.github.com>
2021-04-28 11:24:00 +0200

    Update Projekt3.py

--- Projekt3.py
+++ Projekt3.py
@@ -108,3 +108,20 @@ while True:
         print("Nieprawidłowy znak")
     input()
     os.system("cls")
+    
+    lista1 = [1,2,3,4,5,6]
+def usun_nieparzyste(lista):
+    dlugosc = len(lista)
+    for liczba in lista[:]:
+        if liczba%2 == 1:
+            lista.remove(liczba)
+    return lista
+
+print(usun_nieparzyste(lista1))
+
+lista2 = [7,8,9,10,11,12,13,14,15,16]
+print(usun_nieparzyste(lista2))
+
+lista3 = [1,1,1]
+print(usun_nieparzyste(lista3))
+
